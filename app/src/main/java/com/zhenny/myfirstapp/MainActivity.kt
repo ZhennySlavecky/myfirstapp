f62ec0c40ad972bf3139413947fbaaddd33c874c
unknown <labenu@gmail.com>
2021-12-02 18:20:42 +0300

    init

--- app/src/main/java/com/zhenny/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/zhenny/myfirstapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zhenny.myfirstapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -10,35 +12,47 @@ const val TAG="MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var helloTextView: TextView
-    lateinit var newTextButton: Button
-
+  //  lateinit var helloTextView: TextView
+   // lateinit var newTextButton: Button
+    lateinit var nextActivityButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d(TAG,getString(R.string.str1))
 
-        fun main(): String {
-           val value: String
-           value = ""
-            for (i in 'a'..'z'){
-                if (i.code % 2 == 0) value.append(i)
-            }
-            return value
+        nextActivityButton = findViewById(R.id.center_button)
+        nextActivityButton.setOnClickListener {
+        //    val secondActivityIntent: Intent= Intent(this, SecondActivity::class.java)
+        //    startActivity(secondActivityIntent)
+            val googleLink = Uri.parse("http://google.com")
+            val openBrowserIntent = Intent(Intent.ACTION_VIEW, googleLink)
+            startActivity(openBrowserIntent)
+
         }
 
-        helloTextView = findViewById(R.id.hello)
-        newTextButton = findViewById(R.id.center_button)
+   //     fun main(): String {
+   //        val value: String
+   //        value = ""
+   //         for (i in 'a'..'z'){
+   //             if (i.code % 2 == 0) value.append(i)
+   //         }
+   //         return value
+   //     }
 
-        fun newTextButtonClick() {
-            newTextButton.setOnClickListener {
-                helloTextView.text = main()
-            }
-        }
+        //helloTextView = findViewById(R.id.hello)
+       // newTextButton = findViewById(R.id.center_button)
 
-        newTextButtonClick()
+       // fun newTextButtonClick() {
+        //    newTextButton.setOnClickListener {
+        //        helloTextView.text = main()
+         //   }
+       // }
+
+       // newTextButtonClick()
     }
 
     override fun onStart() {
@@ -68,7 +82,7 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-private fun String.append(i: Char) {
+//private fun String.append(i: Char) {
 
-}
+//}
 
